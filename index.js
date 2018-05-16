6335bc80bc85409fb8c4a154876cb5c05e0604d5
mashur.hossain@gmail.com <mashur.hossain@gmail.com>
2018-05-16 04:38:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ function happyHolidays () {
 }
 
 
+
+
+describe('functions', () => {
+  describe('happyHolidays', () => {
+    it('returns "Happy holidays!"', () => {
+      expect(happyHolidays()).toEqual('Happy holidays!')
+    })
+  })
+
+
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown (days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
