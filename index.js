ad6a304ad94921e995c147db8c3c6823f867b14c
mashur.hossain@gmail.com <mashur.hossain@gmail.com>
2018-05-16 04:41:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 // write your code below!
 function happyHolidays () {
-  return "Happy holidays" 
+  return "Happy holidays!" 
 }
 
 
-
-
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
-
-
-function happyHolidaysTo (name) {
-  return `Happy holidays, ${name}!`
-}
-
-function happyHolidaysTo (name) {
-  return `Happy holidays, ${name}!`
-}
-
-function happyHolidayTo (holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-}
-
-function holidayCountdown (days, holiday) {
-  return `It's ${days} days until ${holiday}!`
-}
